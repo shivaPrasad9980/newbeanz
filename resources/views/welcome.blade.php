0d7fab225f7aa6d15938d4d9cdc70ef78346e163
Shivaprasad <shiva123.sp2@gmail.com>
2024-10-14 12:59:26 +0530

    welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -48,6 +48,7 @@
                                     </svg>
                                 </div>
                                 fguruyufuurts
+                                added another one
                                 
 ddddddddddddddddddddd
                                 <h2 class="mt-6 text-xl font-semibold text-gray-900 dark:text-white">Documentation</h2>

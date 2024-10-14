f0da06ea397cb83c970bf25f2c016cf4206aea1a
Shivaprasad <shiva123.sp2@gmail.com>
2024-10-14 12:27:25 +0530

    Shiva again changed

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -53,6 +53,7 @@
                                 <p class="mt-4 text-gray-500 dark:text-gray-400 text-sm leading-relaxed">
                                     Laravel has wonderful documentation covering every aspect of the framework. Whether you are a newcomer or have prior experience with Laravel, we recommend reading our documentation from beginning to end.
                                 <b>My name is shivaprasad</b>
+                                <b>Shiva changed my code</b>
                                 </p>
                             </div>
 
